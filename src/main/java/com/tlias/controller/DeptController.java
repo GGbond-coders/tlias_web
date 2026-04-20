e3a8ee845a3a5389987d2058c2b4544e55fe8e6b
GGbond <kzhou1655@gmail.com>
2026-04-20 22:45:04 +0800

    day01：部门管理功能

--- src/main/java/com/tlias/controller/DeptController.java
+++ src/main/java/com/tlias/controller/DeptController.java
@@ -2,6 +2,7 @@ package com.tlias.controller;
 
 import com.tlias.pojo.Dept;
 import com.tlias.service.DeptService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
@@ -9,6 +10,7 @@ import java.util.List;
 /**
  * 部门管理 Controller
  */
+@Slf4j
 @RestController
 @RequestMapping("/depts")
 public class DeptController {
@@ -21,7 +23,9 @@ public class DeptController {
      */
     @GetMapping
     public Result list() {
+        log.info("查询所有部门");
         List<Dept> depts = deptService.list();
+        log.info("查询到{}个部门", depts.size());
         return Result.success(depts);
     }
     
@@ -30,7 +34,9 @@ public class DeptController {
      */
     @DeleteMapping("/{id}")
     public Result delete(@PathVariable Integer id) {
+        log.info("删除部门，ID: {}", id);
         deptService.deleteById(id);
+        log.info("删除部门成功，ID: {}", id);
         return Result.success();
     }
     
@@ -39,7 +45,9 @@ public class DeptController {
      */
     @PostMapping
     public Result add(@RequestBody Dept dept) {
+        log.info("新增部门: {}", dept);
         deptService.add(dept);
+        log.info("新增部门成功");
         return Result.success();
     }
     
@@ -48,7 +56,9 @@ public class DeptController {
      */
     @GetMapping("/{id}")
     public Result getById(@PathVariable Integer id) {
+        log.info("根据ID查询部门，ID: {}", id);
         Dept dept = deptService.getById(id);
+        log.info("查询结果: {}", dept);
         return Result.success(dept);
     }
     
@@ -57,7 +67,9 @@ public class DeptController {
      */
     @PutMapping
     public Result update(@RequestBody Dept dept) {
+        log.info("修改部门: {}", dept);
         deptService.update(dept);
+        log.info("修改部门成功");
         return Result.success();
     }
 }
